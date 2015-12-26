5728ee31d6a5eadd21eb04de8655bf954ebec873
Jack <mushank@Gmail.com>
2015-12-26 12:29:22 +0800

    Init Project

---
+++ ZKStandard/ZKStandard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZKStandard
+//
+//  Created by Jack on 12/26/15.
+//  Copyright © 2015 mushank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZKStandard/ZKStandard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZKStandard
+//
+//  Created by Jack on 12/26/15.
+//  Copyright © 2015 mushank. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

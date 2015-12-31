f15221680708e087b2cba25d5bdb8d355aa93e8f
Jack <mushank@Gmail.com>
2015-12-31 14:45:38 +0800

    Add file manager class "ZKFileManager".

---
+++ ZKStandard/ZKStandard/Utilities/ZKFileManager.h
@@ -0,0 +1,13 @@
+//
+//  ZKFileManager.h
+//  ZKStandard
+//
+//  Created by Jack on 12/31/15.
+//  Copyright © 2015 mushank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZKFileManager : NSObject
+
+@end
---
+++ ZKStandard/ZKStandard/Utilities/ZKFileManager.m
@@ -0,0 +1,13 @@
+//
+//  ZKFileManager.m
+//  ZKStandard
+//
+//  Created by Jack on 12/31/15.
+//  Copyright © 2015 mushank. All rights reserved.
+//
+
+#import "ZKFileManager.h"
+
+@implementation ZKFileManager
+
+@end

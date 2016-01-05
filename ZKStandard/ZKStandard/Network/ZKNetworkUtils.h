e2a656c8740852b0a73349ef5bad8e0e8580223f
Jack <mushank@Gmail.com>
2016-01-06 00:10:58 +0800

    Update ZKNetworkUtils

--- ZKStandard/ZKStandard/Network/ZKNetworkUtils.h
+++ ZKStandard/ZKStandard/Network/ZKNetworkUtils.h
@@ -11,11 +11,57 @@
 
 typedef NS_ENUM(NSInteger, ZKServerAddressType)
 {
-    ZKServerAddressTypeDevelopment  = 0, /* 开发环境服务器地址 */
-    ZKServerAddressTypeTest         = 1, /* 测试环境服务器地址 */
-    ZKServerAddressTypeDistribution = 2, /* 生产环境服务器地址 */
+    ZKServerAddressTypeDev  = 0, /* 开发环境 */
+    ZKServerAddressTypeTest = 1, /* 测试环境 */
+    ZKServerAddressTypeDis  = 2, /* 生产环境 */
 };
 
 @interface ZKNetworkUtils : NSObject
+
+/**
+ *  @method             baseServerSchema
+ *  @abstract           get baseServerSchema
+ *  @discussion
+ *  @param              NULL
+ *  @param result       return baseServerSchema String
+ */
++ (NSString *)baseServerSchema;
+
+/**
+ *  @method             baseServerAddressWithType
+ *  @abstract           get baseServerAddress
+ *  @discussion
+ *  @param type         server address type
+ *  @param result       return baseServerAddress String
+ */
++ (NSString *)baseServerAddressWithType:(ZKServerAddressType)type;
+
+/**
+ *  @method             baseServerPath
+ *  @abstract           get baseServerPath
+ *  @discussion
+ *  @param              NULL
+ *  @param result       return baseServerPath String
+ */
++ (NSString *)baseServerPath;
+
+/**
+ *  @method             urlWithSubPath
+ *  @abstract           append baseServerPath with subPath
+ *  @discussion
+ *  @param              NULL
+ *  @param result       return url
+ */
++ (NSURL *)urlWithSubPath:(NSString *)subPath;
+
+/**
+ *  @method             timeOutInterval
+ *  @abstract           get timeOutInterval
+ *  @discussion
+ *  @param              NULL
+ *  @param result       return timeOutInterval doubleValue
+ */
++ (NSTimeInterval)timeOutInterval;
+
 
 @end
--- ZKStandard/ZKStandard/Network/ZKNetworkUtils.m
+++ ZKStandard/ZKStandard/Network/ZKNetworkUtils.m
@@ -7,7 +7,81 @@
 //
 
 #import "ZKNetworkUtils.h"
+#import "ZKFileManager.h"
+
+static NSString *const Config = @"ZKConfig";
+static NSString *const Plist = @"plist";
 
 @implementation ZKNetworkUtils
+
+static NSString *const kBaseServerAddressDev    = @"Base Server Address Dev";   /* <ZKConfig.plish> 开发环境键值 */
+static NSString *const kBaseServerAddressTest   = @"Base Server Address Test";  /* <ZKConfig.plish> 测试环境键值 */
+static NSString *const kBaseServerAddressDis    = @"Base Server Address Dis";   /* <ZKConfig.plish> 生产环境键值 */
+
+static NSString *const kBaseServerSchema        = @"Base Server Schema";    /* <ZKConfig.plish> 服务器Schema键值 */
+static NSString *const kTimeOutInterval         = @"Time Out Interval";     /* <ZKConfig.plish> 网络访问时限键值 */
+
+
++ (NSString *)baseServerSchema
+{
+    NSMutableDictionary *dic = [[ZKFileManager sharedInstance]readFromBundleFile:Config withType:Plist];
+    NSString *baseServierSchema = [dic objectForKey:kBaseServerSchema];
+    return baseServierSchema;
+
+}
+
++ (NSString *)baseServerAddressWithType:(ZKServerAddressType)type
+{
+    NSMutableDictionary *dic = [[ZKFileManager sharedInstance]readFromBundleFile:Config withType:Plist];
+    NSString *baseServerAddress;
+
+    switch (type) {
+        case ZKServerAddressTypeDev:
+            baseServerAddress = [dic objectForKey:kBaseServerAddressDev];
+            break;
+        case ZKServerAddressTypeTest:
+            baseServerAddress = [dic objectForKey:kBaseServerAddressTest];
+            break;
+        case ZKServerAddressTypeDis:
+            baseServerAddress = [dic objectForKey:kBaseServerAddressDis];
+            break;
+        default:
+            break;
+    }
+    
+    return baseServerAddress;
+}
+
++ (NSString *)baseServerPath
+{
+#if DEBUG
+    ZKServerAddressType type = ZKServerAddressTypeDev;
+#else
+    ZKServerAddressType type = ZKServerAddressTypeDis;
+#endif
+    
+    NSString *baseServerSchema = [self baseServerSchema];
+    NSString *baseServerAddress = [self baseServerAddressWithType:type];
+    NSString *baseServerPath = [NSString stringWithFormat:@"%@%@",baseServerSchema,baseServerAddress];
+    
+    return baseServerPath;
+}
+
++ (NSURL *)urlWithSubPath:(NSString *)subPath
+{
+    NSString *urlString = [NSString stringWithFormat:@"%@%@",self.baseServerPath, subPath];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    return url;
+}
+
+
++ (NSTimeInterval)timeOutInterval
+{
+    NSMutableDictionary *dic = [[ZKFileManager sharedInstance]readFromBundleFile:Config withType:Plist];
+    NSTimeInterval timeOutInterval = [[dic objectForKey:kTimeOutInterval] doubleValue];
+    
+    return timeOutInterval;
+}
 
 @end

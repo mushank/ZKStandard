6625a82857ecada89a4bcdccbdc74bcfa0e1fa25
Jack <mushank@Gmail.com>
2016-03-08 13:02:39 +0800

    Add ZKUtils contained string&date casting methods

--- ZKStandard/ZKStandard/Utilities/ZKUtils/ZKUtils.h
+++ ZKStandard/ZKStandard/Utilities/ZKUtils/ZKUtils.h
@@ -10,4 +10,30 @@
 
 @interface ZKUtils : NSObject
 
+#pragma mark - Cast string to date
+// Cast string to date in default formatter @"yyyy-MM-dd HH:mm:ss" at local timezone
++ (NSDate *)dateFromString:(NSString *)string;
+
+// Cast string to date in default formatter @"yyyy-MM-dd HH:mm:ss", TimeZone is @"GMT"
++ (NSDate *)dateInGMTZoneFromString:(NSString *)string;
+
+// Cast string to date in specified formatter,  TimeZone is @"GMT"
++ (NSDate *)dateInGMTZoneFromString:(NSString *)string format:(NSString *)format;
+
+// Cast string to date in specified formatter, specified timezone
++ (NSDate *)dateFromString:(NSString *)string format:(NSString *)format timeZone:(NSString *)timeZone;
+
+#pragma mark - Cast date to string
+// Cast date to string in default formatter @"yyyy-MM-dd HH:mm:ss" at local timezone
++ (NSString *)stringFromDate:(NSDate *)date;
+
+// Cast date to string in default formatter @"yyyy-MM-dd HH:mm:ss", TimeZone is @"GMT"
++ (NSString *)stringFromDateInGMTZone:(NSDate *)date;
+
+// Cast date to string in specified formatter,  TimeZone is @"GMT"
++ (NSString *)stringFromDateInGMTZone:(NSDate *)date format:(NSString *)format;
+
+// Cast date to string in specified formatter, specified timezone
++ (NSString *)stringFromDate:(NSDate *)date format:(NSString *)format timeZone:(NSString *)timeZone;
+
 @end
--- ZKStandard/ZKStandard/Utilities/ZKUtils/ZKUtils.m
+++ ZKStandard/ZKStandard/Utilities/ZKUtils/ZKUtils.m
@@ -8,6 +8,67 @@
 
 #import "ZKUtils.h"
 
+static NSString *kGMTTimeZoneString = @"GMT";
+static NSString *kDefaultDateFormatString = @"yyyy-MM-dd HH:mm:ss";
+
 @implementation ZKUtils
+
+#pragma mark - Cast string to date
++ (NSDate *)dateFromString:(NSString *)string
+{
+    return [self dateFromString:string format:nil timeZone:nil];
+}
+
+
++ (NSDate *)dateInGMTZoneFromString:(NSString *)string
+{
+    return [self dateFromString:string format:nil timeZone:kGMTTimeZoneString];
+}
+
++ (NSDate *)dateInGMTZoneFromString:(NSString *)string format:(NSString *)format
+{
+    return [self dateFromString:string format:format timeZone:kGMTTimeZoneString];
+}
+
++ (NSDate *)dateFromString:(NSString *)string format:(NSString *)format timeZone:(NSString *)timeZone
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:(format ? format : kDefaultDateFormatString)];
+    if (timeZone) {
+        dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:timeZone];
+    }
+    
+    return [dateFormatter dateFromString:string];
+}
+
+#pragma mark - Cast date to string
++ (NSString *)stringFromDate:(NSDate *)date
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:kDefaultDateFormatString];
+    
+    return [dateFormatter stringFromDate:date];
+}
+
++ (NSString *)stringFromDateInGMTZone:(NSDate *)date
+{
+    return [self stringFromDate:date format:nil timeZone:nil];
+}
+
++ (NSString *)stringFromDateInGMTZone:(NSDate *)date format:(NSString *)format
+{
+    return  [self stringFromDate:date format:format timeZone:nil];
+}
+
++ (NSString *)stringFromDate:(NSDate *)date format:(NSString *)format timeZone:(NSString *)timeZone
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:(format ? format : kDefaultDateFormatString)];
+    if (timeZone) {
+        dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:timeZone];
+    }
+    
+    return [dateFormatter stringFromDate:date];
+}
 
 @end

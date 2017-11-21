b60f7e5b3a339aac115b8490f6defaca686186f1
Jack <mushank@Gmail.com>
2017-11-21 10:47:32 +0800

    Recommit xcworkspace

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -42,7 +42,6 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
     [[ZKAPIManager shareInstance] startNetworkMonitoring];
-    [[ZKLocationManager shareInstance] startUpdatingLocation];
 }
 
 
